d54adaffab454ced1de157d721d085fa4d70582e
서벨롭 <pypygeek@gmail.com>
2022-09-16 18:22:19 +0900

    project rename

--- RollDiceApplication/app/src/main/java/com/example/RollDiceApplication/MainActivity.kt
+++ RollDiceApplication/app/src/main/java/com/example/RollDiceApplication/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.RollDiceApplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- RollDiceApplication/app/src/test/java/com/example/RollDiceApplication/ExampleUnitTest.kt
+++ RollDiceApplication/app/src/test/java/com/example/RollDiceApplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.RollDiceApplication
 
 import org.junit.Test
 
